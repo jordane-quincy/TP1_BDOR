6d05544bcaf73fc33f5869c4101528ee7531931e
jordane-quincy <jordanequincy@gmail.com>
2015-12-07 14:55:07 +0100

    Create Tables.sql

---
+++ Tables.sql
@@ -0,0 +1,8 @@
+/* Question 1 - Creation des tables */
+CREATE TABLE Professionnel OF Professionnel_Type (primary key(Num_P));
+
+CREATE TABLE Enseignant OF Enseignant_Type (primary key(Num_Ens))
+	NESTED TABLE Evenements STORE AS tabl_Evenements;
+
+CREATE TABLE Evenement OF Evenement_Type (primary key(Num_E))
+	NESTED TABLE Intervenants STORE AS tabl_Intervenants;
