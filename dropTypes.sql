fc4d7dedfc03271f16e24db04ee25c43c6d45002
JbRezidu <duriez.jeanbaptiste@gmail.com>
2015-12-07 15:03:24 +0100

    Create dropTypes.sql

---
+++ dropTypes.sql
@@ -0,0 +1,7 @@
+drop type Enseignant_Type force;
+drop type Professionnel_Type force;
+drop type Intervenant_REF_Type force;
+drop type Intervenant_REF_TI_Type force;
+drop type Evenement_Type force;
+drop type Evenement_REF_Type force;
+drop type Evenement_REF_TI_Type force;
