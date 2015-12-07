0ea48ad5f8fd4ec50ed93b7c6dfda138d170d040
jordane-quincy <jordanequincy@gmail.com>
2015-12-07 15:35:39 +0100

    Update select.sql

--- select.sql
+++ select.sql
@@ -4,15 +4,14 @@ from table (
   select ev.Intervenants
   from EVENEMENT ev
   where ev.num_e=1
-) e
+) e;
 
 select ev.num_e, cursor(select count(*) from table(ev.intervenants) )
 from EVENEMENT ev;
-
 
 select ev.Evenement.Num_E
 from table (
   select e.Evenements
   from ENSEIGNANT e
   where e.num_ens=1
-) ev
+) ev;
