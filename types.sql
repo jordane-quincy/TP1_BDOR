0f41e24e933a7d2e53937923083ecc1282435ce1
jordane-quincy <jordanequincy@gmail.com>
2015-12-07 15:02:37 +0100

    varchar2(20) vers varchar2(50)

--- types.sql
+++ types.sql
@@ -5,8 +5,8 @@ create type Enseignant_Type;
 --create type Professionnel
 create type Professionnel_Type as object (
   Num_P number,
-  Nom_P varchar2(20),
-  Prenom_P varchar2(20),
+  Nom_P varchar2(50),
+  Prenom_P varchar2(50),
   Entreprise varchar2(50),
   Domaine varchar2(50),
   Contact REF Enseignant_Type
@@ -14,7 +14,7 @@ create type Professionnel_Type as object (
 /
 
 create type Intervenant_REF_Type as object (
-  Filiere varchar2(20),
+  Filiere varchar2(50),
   Enseig REF Enseignant_Type,
   Pro REF Professionnel_Type
 );
@@ -25,10 +25,10 @@ create type Intervenant_REF_TI_Type as table of Intervenant_REF_Type;
 
 create type Evenement_Type as object (
   Num_E number,
-  Nom_E varchar2(20),
+  Nom_E varchar2(50),
   Date_E Date,
-  Lieu varchar2(20),
-  Public_Cible varchar2(20),
+  Lieu varchar2(50),
+  Public_Cible varchar2(50),
   Intervenants Intervenant_REF_TI_Type
 );
 /
@@ -43,9 +43,9 @@ create type Evenement_REF_TI_Type as table of Evenement_REF_Type;
 
 create or replace type Enseignant_Type as object (
   Num_Ens number,
-  Nom_Ens varchar2(20),
-  Adress_Ens varchar2(20),
-  Filiere varchar2(20),
+  Nom_Ens varchar2(50),
+  Adress_Ens varchar2(50),
+  Filiere varchar2(50),
   Evenements Evenement_REF_TI_Type
 );
 /
