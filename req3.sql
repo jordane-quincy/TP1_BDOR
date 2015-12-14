b22c827ef8a3d9671c04c356c24b6371a65690f5
jordane-quincy <jordanequincy@gmail.com>
2015-12-14 14:10:53 +0100

    maj req3

--- req3.sql
+++ req3.sql
@@ -1,4 +1,6 @@
 -- Quels sont les evenements (nom et lieu) auxquels participe l’enseignant n°4
-select e.NUM_ENS, cursor(select ev.Evenement.Nom_E, ev.Evenement.lieu from table(e.Evenements) ev)
-from ENSEIGNANT e
-where e.num_ens=3;
+select t.Evenement.nom_e, t.Evenement.lieu
+from table (select e.Evenements
+            from ENSEIGNANT e
+            where e.Num_Ens=3
+            ) t;
